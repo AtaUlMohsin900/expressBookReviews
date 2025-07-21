7797596e25bdaaeb60e2cade28f49807310bbecf
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-07-21 14:17:49 +0000

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+ return users.some((user) => user.username === username);
+};
 
+const getAllBooks = () => {
+  return books;
+};
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Missing username or password" });
+  } else if (doesExist(username)) {
+    return res.status(404).json({ message: "user already exists." });
+  } else {
+    users.push({ username: username, password: password });
+    return res
+      .status(200)
+      .json({ message: "User successfully registered.  Please login." });
+  }
 });
 
 // Get the book list available in the shop
