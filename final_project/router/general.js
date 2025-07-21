49038c6326c4b936dd02fcf40d3455acb81351c7
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-07-21 14:28:14 +0000

    details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,9 +52,17 @@ ublic_users.get("/isbn/:isbn", async (req, res) => {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get("/author/:author", async (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // get array of matching book objects
+  const matchingBooks = Object.values(await books).filter(
+    (book) => book.author.toLowerCase() === req.params.author.toLowerCase()
+  );
+  if (matchingBooks.length > 0) {
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books by that author." });
+  }
 });
 
 // Get all books based on title
